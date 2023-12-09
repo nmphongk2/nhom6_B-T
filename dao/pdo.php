e2800942704cc94ebc077cab016d448c294ed140
ThaiPhu2k4 <thaiphu2004@gmail.com>
2023-12-09 23:16:59 +0700

    CTP

--- dao/pdo.php
+++ dao/pdo.php
@@ -4,7 +4,7 @@ function pdo_get_connection()
 {
     $dburl = "mysql:host=localhost;dbname=test-duan1;charset=utf8";
     $username = 'root';
-    $password = 'mysql';
+    $password = '';
 
     $conn = new PDO($dburl, $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
